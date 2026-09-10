989a4b42bd4f8c801265343d1dbcdfe60787f97f
suhoveevyuriy <suhoveev.yuriy@gmail.com>
2026-09-10 12:43:32 +0300

    Fixes

--- src/test/java/webShop/SuccessfulRegistration.java
+++ src/test/java/webShop/SuccessfulRegistration.java
@@ -1,4 +1,33 @@
 package webShop;
 
-public class SuccessfulRegistration {
+import base.BaseTest;
+import org.steps.RegisterPageSteps;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SuccessfulRegistration extends BaseTest {
+
+    private RegisterPageSteps registerPageSteps;
+
+    @Test
+    public void verifyRegistration() {
+
+        page.navigate("https://demowebshop.tricentis.com/register");
+
+        String email = "test" + System.currentTimeMillis() + "@gmail.com";
+
+        registerPageSteps.registerMaleUser(
+                "Yurii",
+                "Testovich",
+                email,
+                "Test123@"
+        );
+
+        String actualResult = page.locator(".result").textContent();
+
+        Assert.assertEquals(
+                actualResult,
+                "Your registration completed"
+        );
+    }
 }
